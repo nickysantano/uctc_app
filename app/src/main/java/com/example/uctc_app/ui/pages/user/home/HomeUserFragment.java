34faef806b3a8a98367599bcb6d17369af37d006
nickysantano <nicky.santano@gmail.com>
2020-12-07 16:35:20 +0700

    lauout dasar
    done layout kosong buat user

---
+++ app/src/main/java/com/example/uctc_app/ui/pages/user/home/HomeUserFragment.java
@@ -0,0 +1,25 @@
+package com.example.uctc_app.ui.pages.user.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.uctc_app.R;
+
+public class HomeUserFragment extends Fragment {
+
+    public HomeUserFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_user, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/uctc_app/ui/pages/user/my_program/MyProgramUserFragment.java
@@ -0,0 +1,25 @@
+package com.example.uctc_app.ui.pages.user.my_program;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.uctc_app.R;
+
+public class MyProgramUserFragment extends Fragment {
+
+    public MyProgramUserFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_program_user, container, false);
+    }
+}
