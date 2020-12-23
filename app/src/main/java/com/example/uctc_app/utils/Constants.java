d306bd27858440ca83643945517f3f68e2403df2
nickysantano <nicky.santano@gmail.com>
2020-12-23 17:19:33 +0700

    adding some different roles
    admin, staff main activity

--- app/src/main/java/com/example/uctc_app/utils/Constants.java
+++ app/src/main/java/com/example/uctc_app/utils/Constants.java
@@ -2,8 +2,9 @@ package com.example.uctc_app.utils;
 
 public class Constants {
     //Nicky
-//    public final static String BASE_URL = "http://192.168.43.246/uctcweb/public/api/";
-    //Ray
-    public final static String BASE_URL = "http://192.168.1.75/uctcweb/public/api/";
+    public final static String BASE_URL = "http://192.168.43.246/uctcweb/public/api/";
+
+//    //Ray
+//    public final static String BASE_URL = "http://192.168.1.75/uctcweb/public/api/";
 
 }
