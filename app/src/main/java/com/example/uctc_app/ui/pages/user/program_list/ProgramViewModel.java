36bd96730c356d00aa84c51613e09867e22a37f4
nickysantano <nicky.santano@gmail.com>
2020-12-23 09:54:09 +0700

    some changes
    like always

---
+++ app/src/main/java/com/example/uctc_app/ui/pages/user/program_list/ProgramViewModel.java
@@ -0,0 +1,42 @@
+package com.example.uctc_app.ui.pages.user.program_list;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.uctc_app.model.local.role.Program;
+import com.example.uctc_app.repository.login.ProgramRepository;
+
+import java.util.List;
+
+public class ProgramViewModel extends ViewModel {
+
+    private ProgramRepository repository;
+    private static final String TAG = "ProgramViewModel";
+
+    public ProgramViewModel() {
+
+    }
+
+    public void init(String token) {
+        Log.d(TAG, "init: " + token);
+        repository = ProgramRepository.getInstance(token);
+    }
+
+    public LiveData<List<Program>> getPrograms() {
+        Log.d("Hello","VIewModel");
+        return repository.getPrograms();
+    }
+
+    public LiveData<String> logout() {
+        return repository.logout();
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        Log.d(TAG, "onCleared: ");
+        repository.resetInstance();
+    }
+}
