384c8bcf2fc61fcd807f2c68dcd2c68aabdd9e14
nickysantano <nicky.santano@gmail.com>
2021-01-12 16:16:08 +0700

    linking staff done, code pls
    yes

---
+++ app/src/main/java/com/example/uctc_app/ui/pages/staff/program_list/DetailProgramStaffFragment.java
@@ -0,0 +1,24 @@
+package com.example.uctc_app.ui.pages.staff.program_list;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.uctc_app.R;
+
+public class DetailProgramStaffFragment extends Fragment {
+
+    public DetailProgramStaffFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_program_staff, container, false);
+    }
+}
--- app/src/main/java/com/example/uctc_app/ui/pages/staff/program_list/ProgramStaffFragment.java
+++ app/src/main/java/com/example/uctc_app/ui/pages/staff/program_list/ProgramStaffFragment.java
@@ -3,12 +3,17 @@ package com.example.uctc_app.ui.pages.staff.program_list;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.uctc_app.R;
+import com.example.uctc_app.ui.pages.user.program_list.ProgramUserFragmentDirections;
+
+import butterknife.OnClick;
 
 public class ProgramStaffFragment extends Fragment {
 
@@ -20,5 +25,11 @@ public class ProgramStaffFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_program_staff, container, false);
+    }
+
+    @OnClick({R.id.btn_add_program_list_staff})
+    public void onClick(View view) {
+//        NavDirections action = ProgramUserFragmentDirections.actionNavProgramUserToAddProgramStaffFragment();
+//        Navigation.findNavController(view).navigate(action);
     }
 }
