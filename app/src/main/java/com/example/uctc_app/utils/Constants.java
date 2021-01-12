639b891a041dcb71f43ae02fd474e1a9120d0f49
RayTjan <62584785+RayTjan@users.noreply.github.com>
2021-01-12 08:22:46 +0700

    RoleFilterNavigation

--- app/src/main/java/com/example/uctc_app/utils/Constants.java
+++ app/src/main/java/com/example/uctc_app/utils/Constants.java
@@ -1,8 +1,13 @@
 package com.example.uctc_app.utils;
 
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.uctc_app.ui.pages.user.profile.ProfileUserViewModel;
+
 public class Constants {
+
     //Nicky//
-    public final static String BASE_URL = "http://192.168.43.246/uctcweb/public/api/";
+ //   public final static String BASE_URL = "http://192.168.43.246/uctcweb/public/api/";
 
 //    //Ray
     public final static String BASE_URL = "http://192.168.1.79/api/";
